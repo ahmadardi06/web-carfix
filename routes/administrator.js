c5deb168f01e1d62ba7fdd70494ec65f807b2c99
Ahmad Ardiansyah <ardiansyah3ber@gmail.com>
2018-09-04 13:33:03 +0700

    upload backend carfix

--- routes/administrator.js
+++ routes/administrator.js
@@ -2,8 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/login', function(req, res, next) {
+  res.render('login-backend', { title: 'Sign in to start your session' });
 });
+
+router.post('/login', (req, res, next)=>{
+	var userEmail 		= req.body.userEmail;
+	var userPassword 	= req.body.userPassword;
+
+	var check = {email: userEmail, password: userPassword};
+	console.log(check)
+
+	// if success login
+	if(userEmail && userPassword) {
+		res.redirect('/administrator/dashboard');
+	}
+
+	// error login
+	else{
+		res.render('login-backend', { title: "<span class='label label-danger'>User email or password incorrect !</span>" })
+	}
+})
+
+router.get('/dashboard', (req, res, next)=>{
+	res.render('dashboard-backend')
+})
 
 module.exports = router;
