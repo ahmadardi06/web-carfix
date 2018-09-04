aa7125a1e1528b7dac4fdab8e83c7050e5caa4d2
agung0k <agungkurniawan0n@gmail.com>
2018-09-04 09:08:44 +0700

    ok

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var contactRouter = require('./routes/contact');
+var adminRouter = require('./routes/administrator');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contactus', contactRouter);
+app.use('/administrator', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
