46e68839a34c5d8fcc8b832c3b28aaeeea61ca79
ahmadardi06_ <ahmad.ardi06@gmail.com>
2018-09-05 00:10:40 +0700

    contoh vuejs consumption API

--- configs/database.js
+++ configs/database.js
@@ -1,10 +1,9 @@
-var util = require('util')
 var mysql = require('mysql-model')
 var pool = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'web_carfix'
+    database: 'web_carfix',
 })
 
 module.exports = pool
