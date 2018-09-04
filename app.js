1fda0a6e9e1e9003ac455797abf2f14f847974a5
Ahmad Ardiansyah <ardiansyah3ber@gmail.com>
2018-09-04 16:42:31 +0700

    inisialisasi router dan filenya

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Router for Frontend
 var indexRouter = require('./routes/index');
 var homeRouter = require('./routes/home');
 var usersRouter = require('./routes/users');
@@ -13,8 +14,17 @@ var contactRouter = require('./routes/contact');
 var adminRouter = require('./routes/administrator');
 var tipsntrickRouter = require('./routes/tipsntrick');
 var eventnnewsRouter = require('./routes/eventnnews');
-
 var articleRouter = require('./routes/article');
+
+//Router for Backend
+var slidersRouter = require('./routes/sliders-backend');
+var networksRouter = require('./routes/networks-backend');
+var promoRouter = require('./routes/promo-backend');
+var tipsntrickRouter = require('./routes/tipsntrick-backend');
+var eventnnewsRouter = require('./routes/eventnnews-backend');
+var productsRouter = require('./routes/products-backend');
+var contactusRouter = require('./routes/contactus-backend');
+var usersRouter = require('./routes/users-backend');
 
 var app = express();
 
@@ -28,6 +38,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Route for frontend
 app.use('/', indexRouter);
 app.use('/home', homeRouter);
 app.use('/users', usersRouter);
@@ -37,8 +48,17 @@ app.use('/contactus', contactRouter);
 app.use('/administrator', adminRouter);
 app.use('/tipsntrick', tipsntrickRouter);
 app.use('/eventnnews', eventnnewsRouter);
-
 app.use('/article', articleRouter);
+
+//Route for backend
+app.use('/sliders-backend', slidersRouter);
+app.use('/networks-backend', networksRouter);
+app.use('/promo-backend', promoRouter);
+app.use('/tipsntrick-backend', tipsntrickRouter);
+app.use('/eventnnews-backend', eventnnewsRouter);
+app.use('/products-backend', productsRouter);
+app.use('/contactus-backend', contactusRouter);
+app.use('/users-backend', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
