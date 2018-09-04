3f639db2dc21430beda1c690a76f8cbee93d695d
Ahmad Ardiansyah <ardiansyah3ber@gmail.com>
2018-09-04 17:19:40 +0700

    crud sampai read saja untuk tabel sliders

--- routes/sliders-backend.js
+++ routes/sliders-backend.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var Db = require('../configs/database');
+var tSliders = Db.extend({tableName: "sliders"});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('article', { title: 'Express' });
+  var mSliders = new tSliders();
+	mSliders.find('all', {where: "display = 'y'"}, (err, rows, fields)=>{
+		res.render('sliders-index', {dataSliders: rows});
+	})
 });
+
+router.get('/index', (req, res, next)=>{
+	var mSliders = new tSliders();
+	mSliders.find('all', {where: "display = 'y'"}, (err, rows, fields)=>{
+		res.render('sliders-index', {dataSliders: rows});
+	})
+})
 
 module.exports = router;
